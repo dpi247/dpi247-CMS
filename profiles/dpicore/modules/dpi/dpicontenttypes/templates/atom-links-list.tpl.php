6e8aad06d2078f76cbabfbe02f68821b2f1b5cdd
ddu <ddu@audaxis.com>
2013-10-17 11:35:01 +0200

    Adding ScalD context for dpicontenttypes:
    contexts: dpi_inline and dpi_mediabox with special rendering

--- profiles/dpicore/modules/dpi/dpicontenttypes/templates/atom-links-list.tpl.php
+++ profiles/dpicore/modules/dpi/dpicontenttypes/templates/atom-links-list.tpl.php
@@ -1,7 +1,8 @@
 <aside class="hidden-phone" role="complementary">
-
+  <?php if($content['field_displaytitle']) :?>
   <h2 class="section-title"><?php print drupal_render($content['field_displaytitle'])?></h2>
-  <?php print drupal_render($content['field_linkitems'])?>
+  <?php endif;?>
+    <?php print drupal_render($content['field_linkitems'])?>  
 </aside>
 
 
