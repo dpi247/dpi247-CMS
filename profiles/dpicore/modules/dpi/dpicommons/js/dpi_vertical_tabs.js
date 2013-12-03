9bf59b918e68dde61a610e4db718fcd59d0a163b
tat <tat@tatport>
2013-12-03 12:05:51 +0100

    DPI Commons : nettoyage theme vertical tabs

--- profiles/dpicore/modules/dpi/dpicommons/js/dpi_vertical_tabs.js
+++ profiles/dpicore/modules/dpi/dpicommons/js/dpi_vertical_tabs.js
@@ -4,7 +4,6 @@
     attach: function(context, settings) {
       $('.dpi_vertical_tabs .dpi_vertical_tabs_content').hide();
       $('.dpi_vertical_tabs_pane_title').hide();
-      //$('.dpi_vertical_tabs .dpi_vertical_tabs_content.dpi_vertical_tabs_active').show();
       // On click
       $('.dpi_vertical_tabs_option > a').click(function(e){
         // Select tab item
@@ -15,17 +14,6 @@
         // Show expected pane and tab
         $(this).parent().addClass("dpi_vertical_tabs_active");
         $(this.hash).show();
-        /*
-        // Change URL
-        if(history.pushState) {
-          if(this.hash.endsWith('_0')){
-            history.pushState(null, null, window.location.href);
-          }
-          else {
-            history.pushState(null, null, this.hash);
-          }
-        }
-        */
         return false;
       });
       // Enable one tab on page load
