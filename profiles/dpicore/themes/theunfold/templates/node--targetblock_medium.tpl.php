d7ac36c62fe4100998933b4989bdf34f4b29ebba
odm <odm@audaxis.com>
2014-03-31 11:16:48 +0200

    Templates plus nécessaires

--- profiles/dpicore/themes/theunfold/templates/node--targetblock_medium.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php hide($content['comments']); ?>
-<?php hide($content['links']); ?>
-<?php print drupal_render($content); ?>
-<?php //print render($content['links']); ?>
-<?php //print render($content['comments']); ?>
