3acb776db70291c93ef8a3747cf2933d5e4434ab
odm <odm@audaxis.com>
2014-04-01 17:41:48 +0200

    Theming des targetblocks

--- profiles/dpicore/themes/theunfold/templates/widgets/widget_media.tpl.php
+++ profiles/dpicore/themes/theunfold/templates/widgets/widget_media.tpl.php
@@ -3,7 +3,7 @@
 <h2 class="heading"><?php print $widget['summary'];?></h2>
 <figure><?php print $widget['media'];?></figure>
 <div class="meta">
-		
+
 	<span class="author"><?php print t('Published') ?> 
 	 <?php print $widget['by']; ?>
   	</span>
@@ -11,6 +11,6 @@
   	<time datetime="<?php print $widget['publicationdate'] ?>">
   	  <?php print date('d/m/Y', $widget['publicationdate']).' '.t('at').' '.date('H:i', $widget['publicationdate'])?>
   	</time>
-  	</span>
+  </span>
 
 </div>
