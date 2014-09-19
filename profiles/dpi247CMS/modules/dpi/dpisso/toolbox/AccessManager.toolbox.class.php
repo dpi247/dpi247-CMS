0fb031c7d07b7adf046fce70361db26c051538b1
dimduj <dimduj@76544.no-reply.drupal.org>
2014-09-19 14:31:04 +0200

    SSO first commit

---
+++ profiles/dpi247CMS/modules/dpi/dpisso/toolbox/AccessManager.toolbox.class.php
@@ -0,0 +1,11 @@
+<?php
+class AccessManager{
+  public static function getServicesForAccessToken($accessToken){
+
+  }
+  public static function getServicesForLoginId($loginId){
+
+  }
+  public static function getServices(){}
+
+}
