be618ff408a71a017ad65387c01c958a38aeaf53
César Splète <csp@audaxis.com>
2014-10-28 17:15:16 +0100

    Dpi Comments : création du module et du widget 'signaler un abus' implémentant VotingAPI

---
+++ profiles/dpi247CMS/modules/dpi/dpicomments/theme/dpicomments-reportabuse-widget.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Template implementation to display the comment report abuse widget.
+ */
+?>
+<div class="dpicomments-reportabuse-widget">
+    <?php print $widget_message; ?>
+</div>
