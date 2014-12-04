efa38238e8a6646200c21973effca1f256a673cd
lba <lba@audaxis.com>
2014-12-04 14:58:59 +0100

    Correctif des accroches

--- profiles/dpi247CMS/modules/dpi/dpiblocks/themes/accroche-custom-theme.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiblocks/themes/accroche-custom-theme.tpl.php
@@ -1,7 +1,7 @@
 <div class="dpiblocks-panel dpiblocks-panel-default">
 <?php
 foreach ( $variables ["rows"] as $node ) {
-  '<div class="media">'.print render ( $node ).'</div>';
+  print $node;
 }
 ?>
 </div>
