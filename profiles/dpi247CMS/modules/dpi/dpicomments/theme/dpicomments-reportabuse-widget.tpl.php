c56be21090e933198407206dd85165b7ac91a076
César Splète <csp@audaxis.com>
2014-12-08 17:01:27 +0100

    Suppresion notice/warning commentaires

--- profiles/dpi247CMS/modules/dpi/dpicomments/theme/dpicomments-reportabuse-widget.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpicomments/theme/dpicomments-reportabuse-widget.tpl.php
@@ -4,6 +4,8 @@
  * Template implementation to display the comment report abuse widget.
  */
 ?>
-<div class="dpicomments-reportabuse-widget">
-    <?php print $widget_message; ?>
-</div>
+<?php if (isset($widget_message)) : ?>
+	<div class="dpicomments-reportabuse-widget">
+	    <?php print $widget_message; ?>
+	</div>
+<?php endif; ?>
