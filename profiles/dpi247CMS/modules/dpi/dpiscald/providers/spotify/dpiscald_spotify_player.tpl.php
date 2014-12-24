5c9d69c64972507ce4ce9af536b70200705fb797
César Splète <csp@audaxis.com>
2014-12-24 14:36:12 +0100

    template spotify

--- profiles/dpi247CMS/modules/dpi/dpiscald/providers/spotify/dpiscald_spotify_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/spotify/dpiscald_spotify_player.tpl.php
@@ -1,1 +1,1 @@
-<iframe src="https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+<iframe src="https://embed.spotify.com/<?php print $vars['base_url'] ?>" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
