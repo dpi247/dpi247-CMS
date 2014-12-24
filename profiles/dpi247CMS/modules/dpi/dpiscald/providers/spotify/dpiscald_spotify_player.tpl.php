9e747b0cfdd4508cc5bfacfef96f071ba6a7fee5
César Splète <csp@audaxis.com>
2014-12-24 14:06:53 +0100

    test spotify iframe

--- profiles/dpi247CMS/modules/dpi/dpiscald/providers/spotify/dpiscald_spotify_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/spotify/dpiscald_spotify_player.tpl.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * @file
- *   Default theme implementation for the Scald Storify Player
- */
-?>
-<iframe src="https://embed.spotify.com/?<?php print $vars['base_url'] ?>" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+<iframe src="https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
