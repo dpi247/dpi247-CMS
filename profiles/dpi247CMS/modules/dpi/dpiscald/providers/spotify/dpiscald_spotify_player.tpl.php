6f14a0e68a81fba7c476485d5b7c87d73c491886
César Splète <csp@audaxis.com>
2014-12-24 12:22:30 +0100

    test scald spotify

--- profiles/dpi247CMS/modules/dpi/dpiscald/providers/spotify/dpiscald_spotify_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/spotify/dpiscald_spotify_player.tpl.php
@@ -4,8 +4,4 @@
  *   Default theme implementation for the Scald Storify Player
  */
 ?>
-<div class="storify">
-<iframe  width="435" height="326" src="//storify.com/<?php print $vars['base_url'] ?>/embed?border=false&template=slideshow"
-frameborder=no allowtransparency=true></iframe><script 
-src="//storify.com/<?php print $vars['base_url'] ?>.js?border=false&template=slideshow"></script>
-<noscript>[<a href="//storify.com/<?php print $vars['base_url'] ?>" target="_blank">View the story "<?php print $vars['title'] ?>" on Storify</a>]</noscript></div>
+<span><?php print $vars['base_url'] ?></span>
