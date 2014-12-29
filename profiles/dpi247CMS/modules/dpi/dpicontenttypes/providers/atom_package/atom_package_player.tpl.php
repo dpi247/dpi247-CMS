b1d428504405561cff346ca833116fff776ffc60
lba <lba@audaxis.com>
2014-12-29 15:14:27 +0100

    Modification pour le mode de rendu des noeuds embedé

--- profiles/dpi247CMS/modules/dpi/dpicontenttypes/providers/atom_package/atom_package_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpicontenttypes/providers/atom_package/atom_package_player.tpl.php
@@ -1,12 +1,3 @@
 <div class="<?php print $variables['classes']; ?>">
-  <img class="<?php print $variables['classes']; ?>-img" src="<?php print $variables['vars']['thumbnail'];?>" alt="<?php print $variables['vars']['thumbnail'];?>" />
-  <div class="<?php print $variables['classes']; ?>-content">
-    <div class="<?php print $variables['classes']; ?>-type">
-      <span><?php print $variables['vars']['node']->type; ?></span>
-    </div>
-    <h2><?php print $variables['vars']['node']->title; ?></h2>
-  </div>
-  <div class="<?php print $variables['classes']; ?>-author">
-    <span>Created by <?php print $variables['atom']->data['author'][$variables['atom']->language][0]['name']; ?>.</span>
-  </div>
+  <?php print render($vars['node']); ?>
 </div>
