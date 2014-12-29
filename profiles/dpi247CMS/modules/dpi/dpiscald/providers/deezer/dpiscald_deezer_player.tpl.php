934bba1fcf7cef831683fb2407e5fe16a3c077b6
César Splète <csp@audaxis.com>
2014-12-29 17:13:15 +0100

    Deezer tpl

--- profiles/dpi247CMS/modules/dpi/dpiscald/providers/deezer/dpiscald_deezer_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/deezer/dpiscald_deezer_player.tpl.php
@@ -1,1 +1,1 @@
-<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?autoplay=false&playlist=true&width=700&height=240&cover=true&type=playlist&id=30595446&title=&app_id=undefined" width="700" height="240"></iframe>
+<iframe scrolling="no" frameborder="0" allowTransparency="true" src="<?php print $vars['base_url'] ?>" width="700" height="auto"></iframe>
