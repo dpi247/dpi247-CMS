b5d4f6d8e68b5e5d4f0304106672fbad763e32ad
César Splète <csp@audaxis.com>
2014-12-29 11:49:30 +0100

    Deerzer : test tpl

--- profiles/dpi247CMS/modules/dpi/dpiscald/providers/deezer/dpiscald_deezer_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/deezer/dpiscald_deezer_player.tpl.php
@@ -1,1 +1,1 @@
-TEST DEEZER
+<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?autoplay=false&playlist=true&width=700&height=240&cover=true&type=playlist&id=30595446&title=&app_id=undefined" width="700" height="240"></iframe>
