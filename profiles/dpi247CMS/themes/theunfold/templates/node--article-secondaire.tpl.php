adeaeb931bd74543317e8dd56feeabf0cabe587d
lba <lba@audaxis.com>
2014-12-30 13:40:54 +0100

    dernière modification

---
+++ profiles/dpi247CMS/themes/theunfold/templates/node--article-secondaire.tpl.php
@@ -0,0 +1,11 @@
+<div class="secondary-article-block">
+  <ul>
+  <?php 
+    foreach ($articles as $item) {
+      print '<li>';
+        print $item;
+      print '</li>';
+    }
+  ?>
+  </ul>
+</div>
