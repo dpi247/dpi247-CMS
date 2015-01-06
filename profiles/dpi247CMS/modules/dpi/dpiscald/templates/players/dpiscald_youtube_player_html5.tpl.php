49e750ee937e6f8458dafbef039377477d7b67bb
César Splète <csp@audaxis.com>
2015-01-06 11:00:34 +0100

    Dpi scald : player html5 youtube

---
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_youtube_player_html5.tpl.php
@@ -0,0 +1,1 @@
+<iframe width="<?php print $width; ?>" height="<?php print $height; ?>" src="http://www.youtube.com/embed/<?php print $video_id; ?>?html5=1" frameborder="0" allowfullscreen></iframe>
