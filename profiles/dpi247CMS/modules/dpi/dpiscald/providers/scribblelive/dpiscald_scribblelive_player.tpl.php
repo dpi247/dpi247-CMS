44d822acc3cec84968b6d7bb870a633cfdb43ceb
lba <lba@audaxis.com>
2015-01-16 17:52:43 +0100

    Correctif majeur pour l'importation des medias de types dpiscald

--- profiles/dpi247CMS/modules/dpi/dpiscald/providers/scribblelive/dpiscald_scribblelive_player.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/scribblelive/dpiscald_scribblelive_player.tpl.php
@@ -5,5 +5,5 @@
  */
 ?>
 
-<iframe src="http://embed.scribblelive.com/Embed/v7.aspx?Id=<?php print $vars['id'] ?>"
+<iframe src="<?php print $vars['id'] ?>"
  width='450' height='600' frameborder='0' style='border: 1px solid #000'></iframe>
