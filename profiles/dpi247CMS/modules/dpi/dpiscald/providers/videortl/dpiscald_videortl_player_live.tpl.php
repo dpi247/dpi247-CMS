5ea59995d3f5bbf43670dbd5157fac20e0d25bf8
César Splète <csp@audaxis.com>
2015-02-11 09:37:22 +0100

    DPI Scald video RTL : ajout du theme pour les video live

---
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/videortl/dpiscald_videortl_player_live.tpl.php
@@ -0,0 +1,5 @@
+<?php 
+	$live_id = $video_id = $vars["video_liveid"];
+?>
+<iframe frameborder="0" width="600" height="338" src="http://www.rtl.be/videos/page/rtl-video-en-embed/640.aspx?LiveID=<?php print $video_id ?>"></iframe>
+  	
