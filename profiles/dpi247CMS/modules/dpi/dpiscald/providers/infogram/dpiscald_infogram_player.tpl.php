0f05fb0f2364d3c8015facca29c489e4f0eb7065
César Splète <csp@audaxis.com>
2015-02-18 12:04:47 +0100

    DPI Scald : inforgram provider

---
+++ profiles/dpi247CMS/modules/dpi/dpiscald/providers/infogram/dpiscald_infogram_player.tpl.php
@@ -0,0 +1,4 @@
+<iframe src="//e.infogr.am/<?=$vars['id']?>" width="550" height="1086" scrolling="no" frameborder="0" style="border:none;"></iframe>
+<div style="width:100%;border-top:1px solid #acacac;padding-top:3px;font-family:Arial;font-size:10px;text-align:center;">
+	<a target="_blank" href="<?=$vars['base_url']?>" style="color:#acacac;text-decoration:none;"></a> | <a style="color:#acacac;text-decoration:none;" href="https://infogr.am" target="_blank">Create infographics</a>
+</div>
---
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_infogram_player_html5.tpl.php
@@ -0,0 +1,4 @@
+<script id="infogram_0_<?php print $vars['id']; ?>" src="//e.infogr.am/js/embed.js?s6b" type="text/javascript"></script>
+<div style="width:<?php print $vars['width']; ?>;border-top:1px solid #acacac;padding-top:3px;font-family:Arial;font-size:10px;text-align:center;">
+	<a target="_blank" href="<?php print $vars['base_url']; ?>" style="color:#acacac;text-decoration:none;"></a> | <a style="color:#acacac;text-decoration:none;" href="https://infogr.am" target="_blank">Create infographics</a>
+</div>
