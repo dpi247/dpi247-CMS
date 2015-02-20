a1ae7c8873b287ce8906b9f86ca9ce57f11b1dff
César Splète <csp@audaxis.com>
2015-02-20 11:48:22 +0100

    #13683 Problem provider scribblelive : correction theme

--- profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_scribblelive_player_html5.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_scribblelive_player_html5.tpl.php
@@ -1,2 +1,2 @@
-<iframe src="http://embed.scribblelive.com/Embed/v7.aspx?Id=<?=$id_scribble?>"
- width='<?=$width?>' height='<?=$height?>' frameborder='0' style='border: 1px solid #000'></iframe>
+<iframe src="<?php print $vars['url']; ?>"
+ width='<?php print $vars['width']; ?>' height='<?php print $vars['height']; ?>' frameborder='0' style='border: 1px solid #000'></iframe>
