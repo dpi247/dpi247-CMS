de00bf64466ce67d5413643ce9616668f7775fac
Dimitri Dujardin <dimduj@gmail.com>
2015-03-11 11:13:31 +0100

    Fix issues in comment profile picture path

--- profiles/dpi247CMS/themes/theunfold/templates/comment.tpl.php
+++ profiles/dpi247CMS/themes/theunfold/templates/comment.tpl.php
@@ -1,7 +1,7 @@
 <?php
 if ($comment->picture) {
   $image_style_vars = array(
-    'path' => $comment->picture,
+    'path' => $comment->picture->uri,
     'style_name' => '70x70',
     'width' => 70,
     'height' => 70,
@@ -9,7 +9,7 @@ if ($comment->picture) {
   print theme('image_style', $image_style_vars);
 }
 ?>
-<div class="comments-body">	
+<div class="comments-body">
 	<div class="meta">
 		<span class="author"><?php print t('By')?> <?php print $author?></span>
 		<time datetime="<?php print date('Y-m-d', $comment->changed);?>">
