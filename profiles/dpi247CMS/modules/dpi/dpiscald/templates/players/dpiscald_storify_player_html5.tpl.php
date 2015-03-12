d1d262db108bbc0e547452de3ca1e02c6bb090c8
César Splète <csp@audaxis.com>
2015-03-12 15:44:04 +0100

    Ajout player story et adaptation html

---
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_storify_player_html5.tpl.php
@@ -0,0 +1,22 @@
+<?php /* en 16:9 */ ?>
+<div class="gr-media gr-media--16_9 gr-media--storify">
+    <iframe class="gr-media__content" src="//storify.com/<?php print $vars['base_url'] ?>/embed?border=false" frameborder="0"></iframe>
+    <div class="gr-media__placeholder gr-media__placeholder--info">
+        <a class="gr-media__placeholder-link" href="//storify.com/<?php print $vars['base_url'] ?>/embed?border=false" target="_blank">
+            <span class="icon icon-storify"></span>Voir la vue optimisée
+        </a>
+    </div>
+</div>
+
+<?php 
+/* en 4:3 : 
+<div class="gr-media gr-media--4_3 gr-media--storify">
+    <iframe class="gr-media__content" src="//storify.com/stori/armstrong-avoue-en-mondovision-twitter-se-moque/embed?border=false" frameborder="0"></iframe>
+    <div class="gr-media__placeholder gr-media__placeholder--info">
+        <a class="gr-media__placeholder-link" href="//storify.com/stori/armstrong-avoue-en-mondovision-twitter-se-moque/embed?border=false" target="_blank">
+            <span class="icon icon-storify"></span>Voir la vue optimisée
+        </a>
+    </div>
+</div>
+*/
+?>
