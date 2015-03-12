0ab3c98a41483b14ad5cb6b8505b1d15cfe6a6bf
César Splète <csp@audaxis.com>
2015-03-12 16:02:18 +0100

    Adaptation html youtube

--- profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_youtube_player_html5.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_youtube_player_html5.tpl.php
@@ -5,7 +5,7 @@
 
 <?php /* en 4:3 
 <div class="gr-media gr-media--4_3 gr-media--video gr-media--youtube">
-    <iframe class="gr-media__content" src="http://www.youtube.com/embed/5nBr4Ki7Zqs?html5=1" frameborder="0" allowfullscreen=""></iframe>
+    <iframe class="gr-media__content" src="http://www.youtube.com/embed/<?php print $video_id; ?>?html5=1" frameborder="0" allowfullscreen=""></iframe>
 </div>
 */
 ?>
