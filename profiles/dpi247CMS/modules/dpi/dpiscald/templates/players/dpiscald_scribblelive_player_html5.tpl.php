68d07ad38b3078d839957353aa36c6cbf0d9aceb
César Splète <csp@audaxis.com>
2015-03-13 10:38:25 +0100

    Scribble live adaptation html

--- profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_scribblelive_player_html5.tpl.php
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_scribblelive_player_html5.tpl.php
@@ -1,2 +1,21 @@
-<iframe src="<?php print $vars['url']; ?>"
- width='<?php print $vars['width']; ?>' height='<?php print $vars['height']; ?>' frameborder='0' style='border: 1px solid #000'></iframe>
+<?php /* en 16:9 */ ?>
+<div class="gr-media gr-media--16_9 gr-media--scribble">
+    <iframe class="gr-media__content" src="<?php print $vars['url']; ?>" frameborder="0"></iframe>
+    <div class="gr-media__placeholder gr-media__placeholder--info">
+        <a class="gr-media__placeholder-link" href="<?php print $vars['url']; ?>" target="_blank">
+            <span class="icon icon-scribble"></span>Voir la vue optimisée
+        </a>
+    </div>
+</div>
+
+<?php /* en 4:3 
+<div class="gr-media gr-media--4_3 gr-media--scribble">
+    <iframe class="gr-media__content" src="<?php print $vars['url']; ?>" frameborder="0"></iframe>
+    <div class="gr-media__placeholder gr-media__placeholder--info">
+        <a class="gr-media__placeholder-link" href="<?php print $vars['url']; ?>" target="_blank">
+            <span class="icon icon-scribble"></span>Voir la vue optimisée
+        </a>
+    </div>
+</div>
+*/
+?>
