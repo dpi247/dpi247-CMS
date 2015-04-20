2136faee82b11cb9dc1b5182533af924aa40d332
Barthelemi Laurent <lba@audaxis.com>
2015-04-20 12:15:03 +0200

    ajout fonctionnalité prev next à dpicontenttypes

---
+++ profiles/dpi247CMS/modules/dpi/dpicontenttypes/templates/prev-next.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Default theme for prev next element
+ */
+?>
+<ul class="dpi-prev-next">
+    <li class="dpi-prev">
+        <?php if(isset($variables["prev"])){ ?>
+          <a href="<?php echo (isset($variables["prev"]["alias"]) && $variables["prev"]["alias"]!="") ? $variables["prev"]["alias"] : $variables["prev"]["link"]; ?>">
+            <span class="dpi-glyph-prev"></span>
+            <span class="dpi-prev-link"><?php echo $variables["prev"]["title"] ?></span>
+          </a>        
+        <?php } ?>
+    </li>
+    <li class="dpi-next">
+        <?php if(isset($variables["next"])){ ?>
+          <a href="<?php echo (isset($variables["next"]["alias"]) && $variables["next"]["alias"]!="") ? $variables["next"]["alias"] : $variables["next"]["link"]; ?>">
+              <span class="dpi-glyph-next"></span>
+              <span class="dpi-next-link"><?php echo $variables["next"]["title"] ?></span>
+          </a>        
+        <?php } ?>
+    </li>
+</ul>
