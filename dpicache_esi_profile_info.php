e5009f5af54882fcb8f29c1e9db5cb3028bf170e
Dimitri Dujardin <dimduj@gmail.com>
2015-07-02 09:28:31 +0200

    Prevent WSOD when using a front controller with BOOTSTRAP_SESSION but the call implies rebuilding of cattalos cache plugin http://rossel.audaxis.com/jira/browse/DPICMS-523

--- dpicache_esi_profile_info.php
+++ dpicache_esi_profile_info.php
@@ -1,12 +1,11 @@
 <?php
 
-
-
-
 define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
 global $user;
+
+load_all_module_using_ctools_plugin();
 
 
 
@@ -18,3 +17,41 @@ $username = $user->uid ? $user->name : "anonymous";
 drupal_add_http_header('X-Dpicache-userid', $user->uid);
 drupal_add_http_header('X-Dpicache-username', $username);
 drupal_add_http_header('X-Dpicache-roles', $role_string);
+
+
+
+function load_all_module_using_ctools_plugin()
+{
+
+	/*
+	  You should maintain this list manually .
+	  All the keys of this call should belong to the array:
+
+	  module_load_include('inc','ctools','includes/plugins');
+	  dsm(ctools_get_plugins_info());
+	*/
+
+	$modules_that_uses_ctools_plugin_system = array(
+		'ctools',
+		'dpiblocks',
+		'dpicache',
+		'dpimport',
+		'dpisocial',
+		'dpisso',
+		'enabootstrap_companion',
+		'entityreference',
+		'panels',
+		'views_bulk_operations',
+		'page_manager',
+		'addressfield',
+		//'feeds',
+		//'spaces',
+		//'context',
+		//'quicktabs',
+
+	);
+
+	foreach ($modules_that_uses_ctools_plugin_system as $module_name) {
+		module_load_include('module', $module_name);
+	}
+}
