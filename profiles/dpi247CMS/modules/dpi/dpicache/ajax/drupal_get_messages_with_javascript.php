90aeb2948982021a825a4ff56240dfe117c3ffb6
Barthelemi Laurent <lba@audaxis.com>
2015-07-23 15:58:39 +0200

    modification de l'emplacement de l'ajax

--- profiles/dpi247CMS/modules/dpi/dpicache/ajax/drupal_get_messages_with_javascript.php
+++ profiles/dpi247CMS/modules/dpi/dpicache/ajax/drupal_get_messages_with_javascript.php
@@ -1,41 +1,25 @@
 <?php
-
-/*
+/**
  * This file is used to load messages from Drupal's drupal_get_message()
  *
  * and additional argument allow to retrive the format in JSON for future purpose
  * Like (Headless RESTFul Server or mobile application)
  * use the url drupal_get_messages_with_javascript.php?format=json
- *
- *
- *
- *
  */
-
-
-
-
-
 define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/includes/theme.inc';
-
 drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
-
 load_all_module_using_ctools_plugin();
-
 
 $variables['display']=NULL;
 print drupal_get_status_messages_with_javascript($variables);
 
-
-
-
-
-
+/**
+ * Load plugin ctools needed to process elements.
+ */
 function load_all_module_using_ctools_plugin()
 {
-
 	/*
 	  You should maintain this list manually .
 	  All the keys of this call should belong to the array:
@@ -57,11 +41,6 @@ function load_all_module_using_ctools_plugin()
 		'views_bulk_operations',
 		'page_manager',
 		'addressfield',
-		//'feeds',
-		//'spaces',
-		//'context',
-		//'quicktabs',
-
 	);
 
 	foreach ($modules_that_uses_ctools_plugin_system as $module_name) {
@@ -73,7 +52,11 @@ function load_all_module_using_ctools_plugin()
 	}
 }
 
-
+/**
+ * This function generate HTML Drupal Message
+ * @param Array $variables
+ * @return String
+ */
 function drupal_get_status_messages_with_javascript($variables) {
 	$display = $variables ['display'];
 	$output = '';
