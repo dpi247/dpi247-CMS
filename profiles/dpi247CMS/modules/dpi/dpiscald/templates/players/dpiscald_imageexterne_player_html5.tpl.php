e92b312693392f83bde88f2218d53433510fb6d8
César Splète <csp@audaxis.com>
2015-08-31 16:24:56 +0200

    Image externe scald provider

---
+++ profiles/dpi247CMS/modules/dpi/dpiscald/templates/players/dpiscald_imageexterne_player_html5.tpl.php
@@ -0,0 +1,1 @@
+<img src="<?php print $vars['url'];?>" alt="<?php print $vars['title'];?>" title="<?php print $vars['title'];?>" width="<?php print $vars['width'];?>" height="<?php print $vars['height'];?>" />
